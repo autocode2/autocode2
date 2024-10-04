ec3af5f2d635db58fd3f1c325cd820e0965b3c9c
Gareth Andrew <gingerhendrix@gmail.com>
2024-10-05 00:29:51 +0100

    Add open in editor and exit menu commands

--- packages/auto-code-ink/src/App.tsx
+++ packages/auto-code-ink/src/App.tsx
@@ -1,9 +1,11 @@
+import { edit } from "external-editor";
 import React, { useEffect } from "react";
 import { CodeAgent, CommandConfig } from "@autocode2/core";
-import { Box, Static, Text } from "ink";
-import TextInput from "ink-text-input";
+import { Box, Static, Text, useApp, useInput } from "ink";
+import TextInput from "./components/TextInput.js";
 import StaticMessageDisplay, { type StaticMessage } from "./StaticMessage.js";
 import { v4 as uuid } from "uuid";
+import { Select } from "@inkjs/ui";
 
 export default function App({
   config,
@@ -13,12 +15,14 @@ export default function App({
   codeAgent: CodeAgent;
 }) {
   const [firstRun, setFirstRun] = React.useState(true);
+  const [showMenu, setShowMenu] = React.useState(false);
   const [message, setMessage] = React.useState("");
   const [messages, setMessages] = React.useState<StaticMessage[]>([]);
   const appendMessage = (message: StaticMessage) =>
     setMessages((messages) => [...messages, message]);
 
   const modelName = config.getModelName();
+  const { exit } = useApp();
 
   useEffect(() => {
     codeAgent.on("response", (response) => {
@@ -42,6 +46,26 @@ export default function App({
     await codeAgent.run(message);
   }
 
+  async function selectMenuItem(value: string) {
+    setShowMenu(false);
+    if (value === "exit") {
+      exit();
+      return;
+    }
+    if (value === "editor") {
+      const update = edit(message, {
+        postfix: ".md"
+      });
+      setMessage(update);
+    }
+  }
+
+  useInput((input, key) => {
+    if (key.ctrl && input === "`") {
+      setShowMenu((show) => !show);
+    }
+  });
+
   return (
     <Box flexDirection="column">
       <Static items={messages}>
@@ -50,17 +74,36 @@ export default function App({
         )}
       </Static>
       {firstRun && <Text>Welcome to Autocode</Text>}
-      <Box flexDirection="row" gap={2}>
-        <Text color="blue">{">>>"}</Text>
-        <Box flexGrow={1}>
-          <TextInput
-            onChange={setMessage}
-            value={message}
-            onSubmit={sendMessage}
+      {!showMenu && (
+        <Box flexDirection="row" gap={2}>
+          <Text color="blue">{">>>"}</Text>
+          <Box flexGrow={1}>
+            <TextInput
+              onChange={setMessage}
+              value={message}
+              onSubmit={sendMessage}
+            />
+          </Box>
+          <Text>{modelName}</Text>
+        </Box>
+      )}
+      {showMenu && (
+        <Box padding={2} flexDirection="column" gap={1}>
+          <Select
+            options={[
+              {
+                label: "Open in editor",
+                value: "editor"
+              },
+              {
+                label: "Exit",
+                value: "exit"
+              }
+            ]}
+            onChange={selectMenuItem}
           />
         </Box>
-        <Text>{modelName}</Text>
-      </Box>
+      )}
     </Box>
   );
 }
