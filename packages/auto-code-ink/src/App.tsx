238b7428b75fe083a8648d4986adfc57f07629d3
Gareth Andrew <gingerhendrix@gmail.com>
2024-10-04 23:19:58 +0100

    Fairly nice chat interface

--- packages/auto-code-ink/src/App.tsx
+++ packages/auto-code-ink/src/App.tsx
@@ -1,56 +1,66 @@
 import React, { useEffect } from "react";
 import { CodeAgent, CommandConfig } from "@autocode2/core";
 import { Box, Static, Text } from "ink";
-import TextInput from "ink-text-input"
-
-type Message = {
-  id: string;
-  content: string;
-};
+import TextInput from "ink-text-input";
+import StaticMessageDisplay, { type StaticMessage } from "./StaticMessage.js";
+import { v4 as uuid } from "uuid";
 
 export default function App({
-  config: _config,
-  codeAgent: codeAgent,
+  config,
+  codeAgent
 }: {
-  config: CommandConfig,
-  codeAgent: CodeAgent,
+  config: CommandConfig;
+  codeAgent: CodeAgent;
 }) {
   const [firstRun, setFirstRun] = React.useState(true);
   const [message, setMessage] = React.useState("");
-  const [messages, setMessages] = React.useState<Message[]>([]);
+  const [messages, setMessages] = React.useState<StaticMessage[]>([]);
+  const appendMessage = (message: StaticMessage) =>
+    setMessages((messages) => [...messages, message]);
+
+  const modelName = config.getModelName();
 
   useEffect(() => {
-    codeAgent.on("response", (response) => console.log(response))
+    codeAgent.on("response", (response) => {
+      appendMessage({
+        id: uuid(),
+        response,
+        type: "ai"
+      });
+    });
   }, [codeAgent]);
 
   async function sendMessage(message: string) {
+    if (!message || message == "") return;
     setFirstRun(false);
     setMessage("");
-    setMessages([...messages, { id: `${Math.random()}`, content: message }]);
-    //await codeAgent.run(message);
+    appendMessage({
+      id: uuid(),
+      content: message,
+      type: "human"
+    });
+    await codeAgent.run(message);
   }
 
   return (
     <Box flexDirection="column">
       <Static items={messages}>
-				{message => (
-					<Box key={message.id}>
-						<Text color="green">✔ {message.content}</Text>
-					</Box>
-				)}
-			</Static>
-      {firstRun && (
-        <Text>
-          Welcome to Autocode
-        </Text>
-      )}
+        {(message) => (
+          <StaticMessageDisplay message={message} key={message.id} />
+        )}
+      </Static>
+      {firstRun && <Text>Welcome to Autocode</Text>}
       <Box flexDirection="row" gap={2}>
         <Text color="blue">{">>>"}</Text>
         <Box flexGrow={1}>
-          <TextInput onChange={setMessage} value={message} onSubmit={sendMessage} />
+          <TextInput
+            onChange={setMessage}
+            value={message}
+            onSubmit={sendMessage}
+          />
         </Box>
-        <Text>claude</Text>
+        <Text>{modelName}</Text>
       </Box>
     </Box>
   );
-};
+}
